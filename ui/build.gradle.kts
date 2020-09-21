bf7ab78ba5b248e302be7da98d8a576dc12e71f7
Roman Khomenko <romankhomenko1995@gmail.com>
2020-09-21 20:09:35 +0300

    Added JavaFX example

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -1,5 +1,12 @@
 plugins {
     kotlin("jvm") version "1.4.10"
+    application
+    id("org.openjfx.javafxplugin") version "0.0.8"
+}
+
+javafx {
+    version = "14"
+    modules = listOf("javafx.controls")
 }
 
 group = "org.khomenko.crypto.labs.ui"
--- ui/src/main/kotlin/org/khomenko/crypto/labs/ui/main/Application.kt
+++ ui/src/main/kotlin/org/khomenko/crypto/labs/ui/main/Application.kt
@@ -1,5 +1,30 @@
 package org.khomenko.crypto.labs.ui.main
 
-fun main() {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+class HelloFX : Application() {
+    override fun start(stage: Stage) {
+        val javaVersion = System.getProperty("java.version")
+        val javafxVersion = System.getProperty("javafx.version")
+        val l = Label("Hello, JavaFX $javafxVersion, running on Java $javaVersion.")
+        val scene = Scene(StackPane(l), 640.toDouble(), 480.toDouble())
+        stage.scene = scene
+        stage.show()
+    }
+
+    companion object {
+        @JvmStatic
+        fun run(args: Array<String>) {
+            launch(HelloFX::class.java, *args)
+        }
+    }
+}
+
+fun main(args: Array<String>) {
     println("Hello from Kotlin")
+    HelloFX.run(args);
 }
