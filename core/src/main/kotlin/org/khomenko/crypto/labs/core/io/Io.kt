0220f3552f173980a703c292cdfe25e3382b6fbd
Roman Khomenko <romankhomenko1995@gmail.com>
2020-09-30 22:45:33 +0300

    Task02 done

--- core/src/main/kotlin/org/khomenko/crypto/labs/core/io/Io.kt
+++ core/src/main/kotlin/org/khomenko/crypto/labs/core/io/Io.kt
@@ -2,11 +2,11 @@ package org.khomenko.crypto.labs.core.io
 
 import kotlin.system.exitProcess
 
-fun <T> readUntil(chances: Int, reader: () -> T): T {
+fun <T> readUntil(chances: Int, converter: (String) -> T): T {
     var value: T? = null
     for (i in 1..chances) {
         try {
-            value = reader()
+            value = converter(readLine()!!)
             break
         } catch (e: NumberFormatException) {
             println("Bad format. Try again:")
--- lab01/src/main/kotlin/org/khomenko/crypto/labs/lab01/Task02.kt
+++ lab01/src/main/kotlin/org/khomenko/crypto/labs/lab01/Task02.kt
@@ -1,5 +1,24 @@
 package org.khomenko.crypto.labs.lab01
 
-fun main() {
+import org.khomenko.crypto.labs.core.bitutils.concatFirstLastBits
+import org.khomenko.crypto.labs.core.bitutils.extractBetweenFirstLastBits
+import org.khomenko.crypto.labs.core.ext.toBinString
+import org.khomenko.crypto.labs.core.ext.toBinUInt
+import org.khomenko.crypto.labs.core.io.readUntil
 
+@ExperimentalUnsignedTypes
+fun main() {
+    val chances = 3
+
+    println("Enter a 32-bit number:")
+    val a = readUntil(chances, String::toBinUInt)
+
+    println("Enter number of firs/last bits i:")
+    val i = readUntil(chances, String::toInt)
+
+    val firstLastConcat = concatFirstLastBits(a, i)
+    println("Concatenation of firs/last i bits: ${firstLastConcat.toBinString()}")
+
+    val betweenBits = extractBetweenFirstLastBits(a, i)
+    println("Bits between first/last i bist: ${betweenBits.toBinString()}")
 }
