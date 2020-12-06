83b93cfca1b25d71baed66af1065d3da594b631c
Roman Khomenko <romankhomenko1995@gmail.com>
2020-12-06 21:32:50 +0300

    Lab03: Added Task02 demo

---
+++ lab03/src/main/kotlin/org/khomenko/crypto/labs/lab03/Task02.kt
@@ -0,0 +1,14 @@
+package org.khomenko.crypto.labs.lab03
+
+import org.khomenko.crypto.labs.core.io.readUntil
+import org.khomenko.crypto.labs.core.number.algo.greatestCommonDivisorExt
+
+fun main() {
+    println("Execute extended GCD algorithm on a, b. Enter a, b:")
+    val a = readUntil(3, String::toInt)
+    val b = readUntil(3, String::toInt)
+
+    val (r, x, y) = greatestCommonDivisorExt(a, b)
+    println("Result: r=${r}, x=${x}, y=${y}")
+    println("r == a * x + b * y = ${a * x + b * y}")
+}
